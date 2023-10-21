6669c0ceb9573d35dabd0764efe8740304b713d4
abtirish <abtirish@gmail.com>
2023-10-20 18:40:26 -0600

    finish phase 1 array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "1998", result: "W"},
+]
+
+//Find a specific result of W
+function superbowlWin(record) {
+    const wonRecord = record.find(entry => entry.result === "W");
+    if (wonRecord) {
+        return wonRecord.year;
+    }
+    
+}
+
+console.log(superbowlWin(record));
+
+
+
+
+
+
+
